f6877007d215296ea30cebe2693c0afb0cc028ce
kusabashira <kusabashira227@gmail.com>
2015-08-29 17:18:30 +0900

    Add completion

---
+++ rakudobrew.bash
@@ -0,0 +1,33 @@
+_rakudobrew() {
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    COMPREPLY=( $( compgen -W 'current list list-available
+      build build-panda triple rehash switch nuke
+      self-upgrade test' -- "$cur") )
+  elif [ "$COMP_CWORD" -ge 2 ]; then
+    case "${COMP_WORDS[1]}" in
+      build)
+        case "$cur" in
+          -*)
+            COMPREPLY=('--configure-opts=')
+            compopt -o nospace
+            ;;
+          *)
+            COMPREPLY=( $( compgen -W 'jvm glr moar all' -- "$cur") )
+            ;;
+        esac
+        ;;
+      switch)
+        COMPREPLY=( $( compgen -W 'jvm glr moar' -- "$cur") )
+        ;;
+      nuke)
+        COMPREPLY=( $( compgen -W 'jvm glr moar' -- "$cur") )
+        ;;
+      test)
+        COMPREPLY=( $( compgen -W 'jvm glr moar all' -- "$cur") )
+        ;;
+    esac
+  fi
+}
+complete -F _rakudobrew rakudobrew
