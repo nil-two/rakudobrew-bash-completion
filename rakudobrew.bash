db1744e1849327ab3485e345f7e201dadb9d497b
kusabashira <kusabashira227@gmail.com>
2015-09-19 23:45:19 +0900

    Fix invalid indent

--- rakudobrew.bash
+++ rakudobrew.bash
@@ -68,15 +68,15 @@ _rakudobrew() {
   case "$COMP_CWORD" in
     1) _rakudobrew_commands ;;
     *) case "${COMP_WORDS[1]}" in
-        build)  _rakudobrew_build ;;
-        switch) _rakudobrew_backends ;;
-        nuke)   _rakudobrew_backends ;;
-        test)   _rakudobrew_backends --allow-all ;;
-        local)  _rakudobrew_versions ;;
-        global) _rakudobrew_versions ;;
-        which)  _rakudobrew_which ;;
-        whence) _rakudobrew_whence ;;
-      esac ;;
+         build)  _rakudobrew_build ;;
+         switch) _rakudobrew_backends ;;
+         nuke)   _rakudobrew_backends ;;
+         test)   _rakudobrew_backends --allow-all ;;
+         local)  _rakudobrew_versions ;;
+         global) _rakudobrew_versions ;;
+         which)  _rakudobrew_which ;;
+         whence) _rakudobrew_whence ;;
+       esac ;;
   esac
 }
 complete -F _rakudobrew rakudobrew
